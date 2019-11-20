cb41044435aa0f368b668bbf4f3866296bb60b64
DWhips <danielwhipple940@gmail.com>
2019-11-20 09:49:51 -0600

    Contours now print onto the contour image

--- Code/ArteryDiamTest.py
+++ Code/ArteryDiamTest.py
@@ -14,6 +14,9 @@ sample_start_row = 144  # measurements are based off the 640x480 sample image
 sample_end_row = 408
 sample_start_col = 159
 sample_end_col = 518
+# colors
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
 
 # Pull image ###################################
 print("Getting image from ", image_file_name, "\n")
@@ -34,18 +37,23 @@ while success:  # this is where big files might mess up if every frame is saved
 
     # start processing the image
     # -----Just trying to draw around artery-------
-    img = cv2.imread(temp_image_file_path + "frame%i.jpg" % i_frame, cv2.IMREAD_GRAYSCALE)
-    _, threshold = cv2.threshold(img, 150, 200, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # SIMPLE vs NONE
 
-    for cnt in contours:
-        cv2.drawContours(img, [cnt], 0, (168, 50, 50), 5) # (168, 50, 50) is red
-        cv2.imshow("image", img)
-        cv2.imshow("thresh", threshold)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    # might want to greyscale the image, see if it improves contours once they work
+    img = cv2.imread(temp_image_file_path + "frame%i.jpg" % i_frame, cv2.IMREAD_GRAYSCALE)
+    hierarchy, threshold = cv2.threshold(img, 120, 200, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # TREE vs EXTERNAL & SIMPLE vs NONE
+    print("n Contours found : ", str(len(contours)))
+
+    # for cnt in contours:
+    cv2.imshow("Image Without Contours", image)  # contour is also destructive
+    cv2.drawContours(img, contours, -1, GREEN, 2)  # this image should be showing lines around high contrast areas
+    cv2.imshow("Image With Contours", img)
+    cv2.imshow("Threshold Image", threshold)
+    # cv2.imshow("contour", threshold)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     # ---------------------------------------------
 
-    print("File %i finished" % i_frame)
+    print("Image %i Complete" % i_frame, "\n")
     i_frame += 1
     success, image = artery_vid.read()
