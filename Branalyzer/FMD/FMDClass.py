50f18a402ae5d231ac798fca1a55a715af590f23
SpoonerDan <39391301+SpoonerDan@users.noreply.github.com>
2020-02-20 16:53:50 -0600

    Added Class
    classFMD

---
+++ Branalyzer/FMD/FMDClass.py
@@ -0,0 +1,6 @@
+# This class will calculate the flow-mediated dilation for a user-input test (test_name) and
+# user-input file (file_path).
+
+
+class classFMD:
+    def __init__(self, test_name, file_path):
