f12f47d51797e5f12dfad0ad04038cd81dfaa84c
DWhips <danielwhipple940@gmail.com>
2020-04-26 16:49:51 -0500

    Marks conf frames in list as red

--- Banalyzer/GUI/confidence_screen.py
+++ Banalyzer/GUI/confidence_screen.py
@@ -28,8 +28,8 @@ class Ui_confidence_screen(QWidget):
         height = QDesktopWidget().screenGeometry(-1).height()
 
         # width and height of the created window
-        windowwidth = width/2
-        windowheight = height/1.5
+        windowwidth = width / 2
+        windowheight = height / 1.5
 
         # Create the confidence screen widget
         self.confidence_screen = QtWidgets.QWidget()
@@ -45,13 +45,11 @@ class Ui_confidence_screen(QWidget):
         self.crop_image.mousePressEvent = self.GetPos
         self.crop_image.setScaledContents(True)
         self.crop_image.setObjectName("crop_image")
-        # for pixel dimensions
-        image_width = self.crop_image.frameGeometry().width()
-        image_height = self.crop_image.frameGeometry().height()
 
         # Create the Scrolling List
         self.frame_list = QtWidgets.QListWidget(self.confidence_screen)
-        self.frame_list.setGeometry(QtCore.QRect(windowwidth*0.7, windowheight*0.1, windowwidth*0.25, windowheight*0.7))
+        self.frame_list.setGeometry(
+            QtCore.QRect(windowwidth * 0.7, windowheight * 0.1, windowwidth * 0.25, windowheight * 0.7))
         self.frame_list.setStyleSheet("background:rgb(255, 255, 255)")
         self.frame_list.setObjectName("frame_list")
         framefile_list = self.SetListFrames()
@@ -60,15 +58,18 @@ class Ui_confidence_screen(QWidget):
 
         # Make the threshold slider
         self.threshold_slider = QtWidgets.QSlider(self.confidence_screen)
-        self.threshold_slider.setGeometry(QtCore.QRect(windowwidth * 0.1, windowheight * 0.85, windowwidth * 0.8, windowheight * 0.1))
+        self.threshold_slider.setGeometry(
+            QtCore.QRect(windowwidth * 0.1, windowheight * 0.85, windowwidth * 0.8, windowheight * 0.1))
         self.threshold_slider.setOrientation(QtCore.Qt.Horizontal)
         self.threshold_slider.setObjectName("threshold_slider")
         self.threshold_slider.setRange(0, 255)
-        self.threshold_slider.valueChanged.connect(lambda: self.SliderChanged(pix_frames_sorted[self.frame_list.currentRow()]))
+        self.threshold_slider.valueChanged.connect(
+            lambda: self.SliderChanged(pix_frames_sorted[self.frame_list.currentRow()]))
 
         # Create the screen title
         self.screen_title = QtWidgets.QLabel(self.confidence_screen)
-        self.screen_title.setGeometry(QtCore.QRect(windowwidth*0.1, windowheight*0.05, windowwidth*0.6, windowheight*0.1))
+        self.screen_title.setGeometry(
+            QtCore.QRect(windowwidth * 0.1, windowheight * 0.05, windowwidth * 0.6, windowheight * 0.1))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.screen_title.setFont(font)
@@ -77,19 +78,22 @@ class Ui_confidence_screen(QWidget):
 
         # Create the Accept Button
         self.accept_btn = QtWidgets.QPushButton(self.confidence_screen)
-        self.accept_btn.setGeometry(QtCore.QRect(windowwidth*0.2, windowheight*0.7, windowwidth*0.1, windowheight*0.05))
+        self.accept_btn.setGeometry(
+            QtCore.QRect(windowwidth * 0.2, windowheight * 0.7, windowwidth * 0.1, windowheight * 0.05))
         self.accept_btn.setStyleSheet("background:rgb(255, 255, 255)")
         self.accept_btn.setObjectName("accept_btn")
 
         # Discard Button
         self.discard_btn = QtWidgets.QPushButton(self.confidence_screen)
-        self.discard_btn.setGeometry(QtCore.QRect(windowwidth*0.4, windowheight*0.7, windowwidth*0.1, windowheight*0.05))
+        self.discard_btn.setGeometry(
+            QtCore.QRect(windowwidth * 0.4, windowheight * 0.7, windowwidth * 0.1, windowheight * 0.05))
         self.discard_btn.setStyleSheet("background:rgb(255, 255, 255)")
         self.discard_btn.setObjectName("discard_btn")
 
         # Create the Done Button
         self.done_btn = QtWidgets.QPushButton(self.confidence_screen)
-        self.done_btn.setGeometry(QtCore.QRect(windowwidth*0.88, windowheight*0.93, windowwidth*0.1, windowheight*0.05))
+        self.done_btn.setGeometry(
+            QtCore.QRect(windowwidth * 0.88, windowheight * 0.93, windowwidth * 0.1, windowheight * 0.05))
         self.done_btn.setStyleSheet("background:rgb(255, 255, 255)")
         self.done_btn.setObjectName("done_btn")
 
@@ -99,28 +103,30 @@ class Ui_confidence_screen(QWidget):
         self.retranslateUi(self.confidence_screen)
         QtCore.QMetaObject.connectSlotsByName(self.confidence_screen)
 
-
-
+    # when list clicked, change to that index
     def ListClicked(self, pix_array):
         i = self.frame_list.currentRow()
         print(str(i))
         self.LoadFailedFrame(pix_array[i])
 
-
+    # TODO later just have pix arr values stored in gbl class? too big?
+    # gets pixvalues of failed frames
     def GetFailedFrames(self, ff_list):
         # get frames needed
         frames_sorted = fmd_proc.GetiFrameiFilePixels(ff_list)
 
         if len(frames_sorted) == 0:
             print("No diam values are unreasonable")
-        else:# update pixmap to index 0
+        else:  # update pixmap to index 0
             self.LoadFailedFrame(frames_sorted[0])
         return frames_sorted
 
+    # updates the pixmap and processes it
     def LoadFailedFrame(self, pix_array):
-        gbl_fmd.i_class = 0 # TODO make i_class equal to the class given by file
+        gbl_fmd.i_class = 0  # TODO make i_class equal to the class given by file
         fmd_proc.Populate(pix_array, self.crop_image, False)
 
+    # gets user click position
     def GetPos(self, event):
         i_class = gbl_fmd.i_class
         # x = event.pos().x()
@@ -129,6 +135,7 @@ class Ui_confidence_screen(QWidget):
         gbl_fmd.class_list[i_class].UpdateXY(x, y)
         print(gbl_fmd.class_list[i_class].GetXY())
 
+    # updates the processing with new thresholding once adjusted
     def SliderChanged(self, pix_array):
         # TODO make sure gblfmd is right
         i_class = gbl_fmd.i_class
@@ -138,6 +145,7 @@ class Ui_confidence_screen(QWidget):
         # update the image to the current thresh
         self.LoadFailedFrame(pix_array)
 
+    # gets the failed frames index and file index of conf level based on the flag and adds to list
     def SetListFrames(self):
         failed_i_file_arr = []
         failed_i_frame_arr = []
@@ -149,7 +157,10 @@ class Ui_confidence_screen(QWidget):
                     failed_i_file_arr.append(i_file)
                     failed_i_frame_arr.append(i_frame)
                     list_name = "Frame: " + str(i_frame) + " " + file_name
-                    self.frame_list.addItem(list_name)
+                    list_widge = QtWidgets.QListWidgetItem('%s' % list_name)
+                    list_widge.setBackground(QtGui.QColor('red'))
+                    self.frame_list.addItem(list_widge)
+
         return [failed_i_file_arr, failed_i_frame_arr]
 
     def retranslateUi(self, confidence_screen):
